72d11e937c4cafe39f4aa07c7b514e5b473be422
dy <shogun91@naver.com>
2024-08-09 16:32:42 +0900

    레이아웃 진행

--- src/components/default/DefaultHeader.vue
+++ src/components/default/DefaultHeader.vue
@@ -1,6 +1,23 @@
 <template lang="">
     <header>
         <img src="@/assets/images/hanamon_edb_bg.png">
+        <ul class="main_menu">
+            <li>
+                <i class="xi-layout-snb-o"></i>
+            </li>
+            <li>
+                <i class="xi-clock-o"></i>
+            </li>
+            <li>
+                <i class="xi-globus"></i>
+            </li>
+            <li>
+                <i class="xi-search"></i>
+            </li>
+            <li>
+                <i class="xi-document"></i>
+            </li>
+        </ul>
     </header>
 </template>
 <script>
